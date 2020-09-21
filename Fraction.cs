09005d61ace211ac3a5a22dcfd8e1ca721b240a1
crisstels <natalie-hasselmann@web.de>
2020-09-21 10:48:44 +0200

    initial commit

---
+++ Fraction.cs
@@ -0,0 +1,100 @@
+using System;
+
+namespace FractionCalculator
+{
+    class Fraction
+    {
+     private int Denominator { get; set; }
+     private int Counter { get; set; }
+
+     public Fraction()
+     {
+         this.Denominator = 1;
+         this.Counter = 1;
+     }
+
+     public Fraction(int counter, int denominator)
+     {
+         this.Denominator = denominator;
+         this.Counter = counter;
+     }
+     /* Methods for calculating.*/
+     public Fraction Addition(Fraction fraction)
+     {
+         Fraction result = new Fraction();
+         result.Counter = this.Counter * fraction.Denominator+ this.Denominator *fraction.Counter;
+         result.Denominator = this.Denominator * fraction.Denominator;
+         return result;
+     }
+
+     public Fraction Subtraction(Fraction fraction)
+     {
+         Fraction result = new Fraction();
+         result.Counter = this.Counter * fraction.Denominator - this.Denominator * fraction.Counter;
+         result.Denominator = this.Denominator * fraction.Denominator;
+         return result;
+     }
+
+     public Fraction Multiplication(Fraction fraction)
+     {
+         Fraction result = new Fraction();
+         result.Counter= this.Counter * fraction.Counter;
+         result.Denominator = this.Denominator * fraction.Denominator;
+         return result;
+     }
+
+     public Fraction Division(Fraction fraction)
+     {
+         Fraction result = new Fraction();
+         result.Counter = this.Counter * fraction.Denominator;
+         result.Denominator = this.Denominator * fraction.Counter;
+         return result;
+     }
+    /* Allocates result */
+     public Fraction Allocation(Fraction fraction)
+     {
+         Fraction result = new Fraction();
+         this.Counter = result.Counter = fraction.Counter;
+         this.Denominator = result.Denominator = fraction.Denominator;
+         Console.WriteLine(result.Counter);
+         Console.WriteLine(result.Denominator);
+         return result;
+     }
+     /* Detects the greatest common factor */
+     public int Euklid(int counter, int denominator)
+     {
+        if (Counter == 0)
+        {
+            return denominator;
+        }
+        else
+        {
+            while (denominator != 0)
+            {
+                if (counter > denominator)
+                {
+                    counter -= denominator;
+                }
+                else
+                {
+                    counter -= denominator;
+                }
+            }
+        }
+            return counter;
+     }
+    /* Shortens a fraction*/
+     public Fraction shorten()
+     {
+         Fraction fraction = new Fraction();
+         int gcf = Euklid(this.Counter, this.Denominator);
+         fraction.Counter = this.Counter / gcf;
+         fraction.Denominator = this.Denominator/ gcf;
+
+         Console.WriteLine(fraction.Counter);
+         Console.WriteLine(fraction.Denominator);
+
+         return fraction;
+     }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FractionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction result = new Fraction();
+            Fraction fraction1 = new Fraction(3,4);
+            Fraction fraction2 = new Fraction(1,2);
+
+            result.Allocation(fraction1.Addition(fraction2));
+            result.Allocation(result.shorten());
+            Console.Write(result);
+            Console.ReadKey(true);
+        }
+    }
+}
